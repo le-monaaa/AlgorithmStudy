cdee27f8a6a32046cc9902d27949dca4f6089467
le-monaaa <101534897+le-monaaa@users.noreply.github.com>
2024-07-18 16:41:47 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 588 ms, Memory: 58616 KB -BaekjoonHub

---
+++ Python/백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.py
@@ -0,0 +1,37 @@
+import sys
+input = sys.stdin.readline
+
+from collections import deque
+
+def bfs(V, E, R):
+
+    Q = deque([R])
+    cnt = 2
+
+    while Q:
+        u = Q.popleft()
+
+        for v in sorted(E[u], reverse=True):
+            if not visited[v]:
+                visited[v]= cnt
+                cnt += 1
+                Q.append(v)
+
+    return visited
+
+N, M, R = map(int, input().split()) # N: 정점 수, M: 간선 수, R: 시작 정점
+
+graph = [[] for _ in range(N+1)]
+
+visited = [0] * (N+1)
+visited[R] = 1
+
+for _ in range(M):
+    u, v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
+
+bfs(N, graph, R)
+
+for i in range(1, N+1):
+    print(visited[i])
