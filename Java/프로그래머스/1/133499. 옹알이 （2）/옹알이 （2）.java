ee7a9ee76f38b632f44820073e0f133a7a83193f
le-monaaa <101534897+le-monaaa@users.noreply.github.com>
2024-11-29 10:41:46 +0900

    [level 1] Title: 옹알이 (2), Time: 6.01 ms, Memory: 80.2 MB -BaekjoonHub

---
+++ Java/프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int solution(String[] babbling) {
+        int cnt = 0;
+        String[] validWords = {"aya", "ye", "woo", "ma"};
+        for(String talk: babbling) {
+            if(talk.matches("^(aya|ye|woo|ma)+$")) {
+                boolean isValid = true;
+                for(int i = 0; i < validWords.length; i++) {
+                    if(talk.contains(validWords[i] + validWords[i])) {
+                        isValid = false;
+                        break;
+                    }
+                }
+                if(isValid){
+                    cnt++;
+                }
+            }
+        }
+
+        return cnt;
+    }
+}
