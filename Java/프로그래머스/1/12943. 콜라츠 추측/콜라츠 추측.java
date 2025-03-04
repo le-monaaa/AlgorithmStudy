172a7203e8e6cc715aff4d3596b562e548eea574
le-monaaa <101534897+le-monaaa@users.noreply.github.com>
2025-03-04 12:22:04 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.04 ms, Memory: 86 MB -BaekjoonHub

--- Java/프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
+++ Java/프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -1,20 +1,16 @@
 class Solution {
     public int solution(int num) {
         int answer = 0;
-        long num2 = num;
-        if (num2 == 1) {
-            return 0;
-        }
-        while (num2 != 1) {
-            if (answer == 501) {
+        while(num != 1) {
+            if(answer >= 501) {
                 return -1;
             }
-            if (num2 % 2 == 0) {
-                num2 /= 2;
+            if(num%2 == 1) {
+                num = num * 3 + 1;
             } else {
-                num2 = num2 * 3 + 1;
+                num/=2;
             }
-            answer += 1;
+            answer++;
         }
         return answer;
     }
