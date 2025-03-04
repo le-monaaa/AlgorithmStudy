4c6ed1b02fb866531857d1c3f8e7f217e63a412e
le-monaaa <101534897+le-monaaa@users.noreply.github.com>
2025-03-04 15:37:25 +0900

    [level 1] Title: 시저 암호, Time: 1.27 ms, Memory: 81 MB -BaekjoonHub

--- Java/프로그래머스/1/12926. 시저 암호/시저 암호.java
+++ Java/프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -1,14 +1,14 @@
 class Solution {
     public String solution(String s, int n) {
         StringBuilder sb = new StringBuilder();
-
-        for (char c : s.toCharArray()) {
-            if(Character.isLowerCase(c)) {
-                sb.append((char) ('a' + (c - 'a' + n) % 26));
-            } else if (Character.isUpperCase(c)) {
-                sb.append((char) ('A' + (c - 'A' + n) % 26));
-            } else {
-                sb.append(c);
+        for(char c: s.toCharArray()) {
+            if(c == ' ') sb.append(' ');
+            else {
+                if(Character.isUpperCase(c)) {
+                    sb.append((char) ('A' + (c- 'A' + n) % 26));
+                } else {
+                    sb.append((char) ('a' + (c - 'a' + n) % 26));
+                }
             }
         }
         return sb.toString();
