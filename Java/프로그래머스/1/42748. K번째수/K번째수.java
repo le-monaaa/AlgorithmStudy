1d1cab762f70f98e0789c5a3aa61b47ea4868fcd
le-monaaa <101534897+le-monaaa@users.noreply.github.com>
2025-03-06 15:37:37 +0900

    [level 1] Title: K번째수, Time: 3.26 ms, Memory: 75.5 MB -BaekjoonHub

--- Java/프로그래머스/1/42748. K번째수/K번째수.java
+++ Java/프로그래머스/1/42748. K번째수/K번째수.java
@@ -1,15 +1,11 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-
+import java.util.*;
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
-        ArrayList<Integer> answer = new ArrayList<>();
-        for(int i=0; i<commands.length; i++) {
-            int[] sub = Arrays.copyOfRange(array, commands[i][0]-1, commands[i][1]);
-            Arrays.sort(sub);
-            answer.add(sub[commands[i][2]-1]);
+        int[] answer = new int[commands.length];
+        for(int i = 0; i < commands.length; i++) {
+            int[] temp = Arrays.copyOfRange(array, commands[i][0]-1, commands[i][1]);
+            answer[i] = Arrays.stream(temp).sorted().toArray()[commands[i][2]-1];
         }
-
-        return answer.stream().mapToInt(Integer::intValue).toArray();
+        return answer;
     }
 }
