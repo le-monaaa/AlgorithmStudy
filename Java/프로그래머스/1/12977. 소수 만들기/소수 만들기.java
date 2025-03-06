714ca91eda28ea28fc9262509ba2dc7dba8d2c9f
le-monaaa <101534897+le-monaaa@users.noreply.github.com>
2025-03-06 17:52:41 +0900

    [level 1] Title: 소수 만들기, Time: 2.65 ms, Memory: 85.9 MB -BaekjoonHub

--- Java/프로그래머스/1/12977. 소수 만들기/소수 만들기.java
+++ Java/프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -1,23 +1,23 @@
 class Solution {
     public int solution(int[] nums) {
-        int cnt = 0;
-        for(int i = 0; i < nums.length - 2; i++) {
-            for(int j = i + 1; j < nums.length - 1; j ++) {
-                for(int k = j + 1; k < nums.length; k++) {
+        int answer = 0;
+        int len = nums.length;
+
+        for(int i = 0; i < len-2; i++) {
+            for(int j = i + 1; j < len-1; j++) {
+                for(int k = j + 1; k < len; k++) {
                     int sum = nums[i] + nums[j] + nums[k];
-                    if (isPrime(sum)) {
-                        cnt++;
-                    }
+                    if(isPrime(sum)) answer++;
                 }
             }
         }
-        return cnt;
+        return answer;
     }
 
-    public boolean isPrime(int num) {
-        if (num <= 1) return false;
-        for (int i = 2; i <= Math.sqrt(num); i++) {
-            if ( num % i == 0) return false;
+    private boolean isPrime(int num) {
+        if (num < 2) return false;
+        for (int i = 2; i * i <= num; i++) {
+            if(num % i == 0) return false;
         }
         return true;
     }
