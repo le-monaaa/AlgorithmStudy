c68810fc10c621b9e5a3edd8939c12d9709115e5
le-monaaa <101534897+le-monaaa@users.noreply.github.com>
2025-03-06 16:45:12 +0900

    [level 1] Title: 2016년, Time: 1.94 ms, Memory: 74.9 MB -BaekjoonHub

--- Java/프로그래머스/1/12901. 2016년/2016년.java
+++ Java/프로그래머스/1/12901. 2016년/2016년.java
@@ -1,10 +1,10 @@
+import java.time.DayOfWeek;
 import java.time.LocalDate;
-class Solution {
-public String solution(int a, int b) {
 
-    LocalDate date = LocalDate.of(2016, a, b);
-    String dayName = date.getDayOfWeek().name();
-    String answer = dayName.substring(0,3);
-    return answer;
-}
+class Solution {
+    public String solution(int a, int b) {
+        LocalDate date = LocalDate.of(2016, a, b);
+        DayOfWeek day = date.getDayOfWeek();
+        return day.toString().substring(0,3);
+    }
 }
