526620e2f41c6fe78f7c1a1ef629a4599168febc
le-monaaa <101534897+le-monaaa@users.noreply.github.com>
2025-03-06 17:31:13 +0900

    [level 1] Title: 모의고사, Time: 5.37 ms, Memory: 85.4 MB -BaekjoonHub

--- Java/프로그래머스/1/42840. 모의고사/모의고사.java
+++ Java/프로그래머스/1/42840. 모의고사/모의고사.java
@@ -3,28 +3,23 @@ import java.util.Arrays;
 
 class Solution {
     public int[] solution(int[] answers) {
-        int[][] students = {
-                {1, 2, 3, 4, 5},
-                {2, 1, 2, 3, 2, 4, 2, 5},
-                {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}
-        };
-        int[] scores = new int[students.length]; // score
-
-        for(int i=0; i< answers.length; i++) {
-            for (int st = 0; st < students.length; st++) {
-                if (answers[i] == students[st][i % students[st].length]) {
-                    scores[st]++;
-                }
-            }
+        int[] num1 = new int[]{1, 2, 3, 4, 5};
+        int[] num2 = new int[]{2, 1, 2, 3, 2, 4, 2, 5};
+        int[] num3 = new int[]{3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        int[] scores = new int[3];
+        for(int i = 0; i < answers.length; i++) {
+            int ans = answers[i];
+            if(ans == num1[i%5]) scores[0] += 1;
+            if(ans == num2[i%8]) scores[1] += 1;
+            if(ans == num3[i%10]) scores[2] += 1;
         }
-        int maxScore = Arrays.stream(scores).max().orElse(0);
-
-        ArrayList<Integer> answer = new ArrayList<>();
-        for(int i =0; i < scores.length; i++) {
+        int maxScore = Arrays.stream(scores).max().getAsInt();
+        ArrayList<Integer> result = new ArrayList<>();
+        for(int i = 0; i < 3; i++) {
             if(scores[i] == maxScore) {
-                answer.add(i + 1);
+                result.add(i+1);
             }
         }
-        return answer.stream().mapToInt(Integer::intValue).toArray();
+        return result.stream().mapToInt(Integer::intValue).toArray();
     }
 }
