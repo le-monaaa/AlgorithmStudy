6ab49a44c2435a7070b019d2b42c4fa44c95fe32
le-monaaa <101534897+le-monaaa@users.noreply.github.com>
2025-03-18 11:03:27 +0900

    [level 0] Title: 문자열 출력하기, Time: 163.93 ms, Memory: 64.5 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.java
@@ -0,0 +1,9 @@
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        System.out.println(str);
+    }
+}
