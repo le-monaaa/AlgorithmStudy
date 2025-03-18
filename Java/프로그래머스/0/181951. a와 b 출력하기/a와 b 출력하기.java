c593117f0aad4b68fa95d59867bf2a904695d331
le-monaaa <101534897+le-monaaa@users.noreply.github.com>
2025-03-18 11:06:47 +0900

    [level 0] Title: a와 b 출력하기, Time: 399.93 ms, Memory: 82.6 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/181951. a와 b 출력하기/a와 b 출력하기.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] nums = sc.nextLine().split(" ");
+        System.out.println("a = " + nums[0]);
+        System.out.println("b = " + nums[1]);
+    }
+}
