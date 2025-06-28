55edf903d873fd9577a2f64e48cce0ec85c5e87b
le-monaaa <101534897+le-monaaa@users.noreply.github.com>
2025-06-28 21:28:58 +0900

    [Bronze V] Title: 사칙연산, Time: 112 ms, Memory: 14216 KB -BaekjoonHub

---
+++ Java/백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+    
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+    }
+}
