2d70815928d24fa01c82aadb30fbd6e6a648dc43
le-monaaa <101534897+le-monaaa@users.noreply.github.com>
2025-07-24 11:19:54 +0900

    [Bronze V] Title: A/B, Time: 112 ms, Memory: 14264 KB -BaekjoonHub

--- Java/백준/Bronze/1008. A／B/A／B.java
+++ Java/백준/Bronze/1008. A／B/A／B.java
@@ -1,16 +1,14 @@
 import java.io.*;
+import java.util.*;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
+public class Main{
+    public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] nums = br.readLine().split(" ");
-        double num1 = Double.parseDouble(nums[0]);
-        double num2 = Double.parseDouble(nums[1]);
-        double result = num1/num2;
-
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter((System.out)));
-        bw.write(result +"");
-        bw.flush();
-        bw.close();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        double A = Double.parseDouble(st.nextToken());
+        double B = Double.parseDouble(st.nextToken());
+        
+        System.out.println(A / B);
     }
 }
