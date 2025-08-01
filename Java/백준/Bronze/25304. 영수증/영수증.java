cbee64b0e8f97e320dc270ce82238e5d850ea325
le-monaaa <101534897+le-monaaa@users.noreply.github.com>
2025-08-01 20:00:33 +0900

    [Bronze IV] Title: 영수증, Time: 100 ms, Memory: 14188 KB -BaekjoonHub

---
+++ Java/백준/Bronze/25304. 영수증/영수증.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main{
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int x = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            sum += a * b;
+        }
+        if (x == sum) System.out.println("Yes");
+        else System.out.println("No");
+    }
+}
