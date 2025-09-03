8d4b168bc822e7771d883ebf12da8f879335f8d9
le-monaaa <101534897+le-monaaa@users.noreply.github.com>
2025-09-03 17:18:22 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 104 ms, Memory: 14116 KB -BaekjoonHub

--- Java/백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
+++ Java/백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
@@ -1,25 +1,15 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
-
+public class Main{
+    public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] numbers = br.readLine().split(" ");
-        int a = Integer.parseInt(numbers[0]);
-        int b = Integer.parseInt(numbers[1]);
-
-        if (a > b) {
-            System.out.println(">");
-        }
-        else if ( a == b ) {
-            System.out.println("==");
-        }
-        else{
-            System.out.println("<");
-        }
-
-
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        if (a > b) System.out.println(">");
+        else if (a < b) System.out.println("<");
+        else System.out.println("==");
     }
 }
