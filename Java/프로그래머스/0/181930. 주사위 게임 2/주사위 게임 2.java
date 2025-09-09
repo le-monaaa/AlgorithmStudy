af521e850754207784210a5d72b1cf6ea4ca958d
le-monaaa <101534897+le-monaaa@users.noreply.github.com>
2025-09-09 16:46:16 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.03 ms, Memory: 85 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int a, int b, int c) {
+        if (a == b && b == c) {
+            return (a + b + c) * (a * a + b * b + c * c) 
+                * (a * a * a + b * b * b + c * c * c);
+        } else if ((a == b && b != c) || (a == c && b != c) || (b == c && c != a)) {
+            return (a + b + c) * (a * a + b * b + c * c);
+        } else {
+            return a + b + c;
+        }
+    }
+}
