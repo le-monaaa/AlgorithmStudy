fe330af06cdc64223528272ecb83e18076895bd0
le-monaaa <101534897+le-monaaa@users.noreply.github.com>
2025-09-09 17:32:08 +0900

    [level 0] Title: 수 조작하기 1, Time: 7.01 ms, Memory: 75.7 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int n, String control) {
+        for (char c : control.toCharArray()) {
+            if(c == 'w') n += 1;
+            else if ( c == 's') n -= 1;
+            else if (c == 'd') n += 10;
+            else n-= 10;
+        }
+        return n;
+    }
+}   
